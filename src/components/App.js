c202192327e75e956405ea7bd5b85c97245a4f88
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-04-22 23:57:11 +0300

    Lab Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App() {
+  const props = blogData;
+  console.log(props);
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={props.name} />
+      <About about={props.about} image={props.image} />
+      <ArticleList posts={props.posts} />
+      You're on your own from here! Follow
+      the deliverables; test things out in the browser as you write your code;
+      and good luck!
     </div>
   );
 }
